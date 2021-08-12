b9500f31e9ec2e17521242060f936ef244f85828
Karmashov <a.v.karmashov@gmail.com>
2021-08-12 15:03:09 +0300

    Test of scheduled tasks

---
+++ src/main/java/com/wwd/tgdb/scheduled/ScheduledTasks.java
@@ -0,0 +1,51 @@
+package com.wwd.tgdb.scheduled;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.io.File;
+import java.io.IOException;
+
+@EnableScheduling
+public class ScheduledTasks {
+    @Value("${rrc.login}")
+    private String rrcLogin;
+    @Value("${rrc.password}")
+    private String rrcPassword;
+    @Value("${rrc.code}")
+    private String rrcCode;
+    @Value("${rrc.id}")
+    private String rrcId;
+    @Value("${upload.path}")
+    private String uploadPath;
+
+    @Scheduled(fixedDelay = 1800000)
+    public void getGpl() {
+        String command = "curl 'https://b2b.rrc.ru/personal/xml?code=" +
+                rrcCode + "&id=" +
+                rrcId + "&login=" +
+                rrcLogin + "&pass=" +
+                rrcPassword + "'";
+
+        ProcessBuilder processBuilder = new ProcessBuilder(command.split(" "));
+
+        processBuilder.directory(new File(uploadPath + "/"));
+
+        Process process = null;
+        try {
+            process = processBuilder.start();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        int exitCode = process.exitValue();
+
+        System.out.println(exitCode);
+
+        System.out.println(process);
+
+        process.destroy();
+
+        System.out.println(process);
+    }
+}
