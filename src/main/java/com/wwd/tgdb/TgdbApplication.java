3849bd765223a72088b0b7842cc75499b149932d
Karmashov <a.v.karmashov@gmail.com>
2021-08-12 15:43:00 +0300

    Another test of scheduled tasks

--- src/main/java/com/wwd/tgdb/TgdbApplication.java
+++ src/main/java/com/wwd/tgdb/TgdbApplication.java
@@ -3,9 +3,11 @@ package com.wwd.tgdb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.telegram.telegrambots.ApiContextInitializer;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
 import java.util.TimeZone;
 
 @SpringBootApplication
@@ -20,5 +22,10 @@ public class TgdbApplication {
 	@PostConstruct
 	public void init() {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
+	@Scheduled(fixedDelay = 5000)
+	public void scheduledTask() {
+		System.out.println(LocalDateTime.now());
 	}
 }
