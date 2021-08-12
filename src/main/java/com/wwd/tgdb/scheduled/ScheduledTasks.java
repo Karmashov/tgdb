8275793920492789ffcf88d4ede9bf6afe336d8c
Andrey <a.v.karmashov@gmail.com>
2021-08-12 20:35:55 +0300

    Another test of scheduled tasks

--- src/main/java/com/wwd/tgdb/scheduled/ScheduledTasks.java
+++ src/main/java/com/wwd/tgdb/scheduled/ScheduledTasks.java
@@ -26,7 +26,7 @@ public class ScheduledTasks {
 
         ProcessBuilder processBuilder = new ProcessBuilder(command);
 
-        processBuilder.directory(new File("./1.txt"));
+        processBuilder.directory(new File("/home"));
 
         Process process = null;
         try {
