0eb5dfac429f126ee4ad5cabe18a7c89ad43d76a
Karmashov <a.v.karmashov@gmail.com>
2021-08-13 12:13:50 +0300

    Another test of scheduled tasks

--- src/main/java/com/wwd/tgdb/scheduled/ScheduledTasks.java
+++ src/main/java/com/wwd/tgdb/scheduled/ScheduledTasks.java
@@ -2,9 +2,12 @@ package com.wwd.tgdb.scheduled;
 
 import lombok.Data;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Arrays;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 public class ScheduledTasks {
@@ -15,26 +18,39 @@ public class ScheduledTasks {
     private String uploadPath;
 
     public void getGpl() {
-        String command = "curl 'https://b2b.rrc.ru/personal/xml?code=" +
+        String generateCommand = "curl \"https://b2b.rrc.ru/personal/xml?code=" +
                 rrcCode + "&id=" +
                 rrcId + "&login=" +
                 rrcLogin + "&pass=" +
-                rrcPassword + "'";
+                rrcPassword + "\"";
+        String downloadCommand = "wget \"https://b2b.rrc.ru/personal/xml/HK5NqWYgRFwC7yOuEkA3IBUa9Sprs0e1.xml\"";
 
-//        System.out.println(command);
+        System.out.println(generateCommand);
 
+        List<String> commands = new ArrayList<>();
+        commands.add(generateCommand);
+        commands.add(downloadCommand);
 
-//        ProcessBuilder processBuilder = new ProcessBuilder(command.split(" "));
-//
-//        processBuilder.directory(new File("D:/NewFolder/"));
+        ProcessBuilder processBuilder = new ProcessBuilder(commands);
 
-//        Process process = null;
+        processBuilder.directory(new File("D:/NewFolder/"));
+
         Process process = null;
         try {
-            process = Runtime.getRuntime().exec(command);
+            process = Runtime.getRuntime().exec(generateCommand);
 //            process = processBuilder.start();
-            InputStream inputStream = process.getInputStream();
-            System.out.println(Arrays.toString(inputStream.readAllBytes()));
+//            InputStream inputStream = process.getInputStream();
+//            System.out.println(Arrays.toString(inputStream.readAllBytes()));
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+            process.waitFor();
+            process.destroy();
+
+            process = Runtime.getRuntime().exec(downloadCommand);
             process.waitFor();
 //            process.destroy();
         } catch (IOException | InterruptedException exception) {
